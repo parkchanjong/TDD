e4bff587c82085547fe9679669242e050a132a52
parkchanjong <7000cj@gmail.com>
2019-10-14 22:31:26 +0900

    refator avg

--- js/stats.js
+++ js/stats.js
@@ -1,6 +1,10 @@
 exports.max = numbers => Math.max(...numbers);
 exports.min = numbers => Math.min(...numbers);
-exports.avg = numbers => {
-	const sum = numbers.reduce((acc, current) => acc + current, 0);
-	return sum / numbers.length;
-};
+exports.avg = numbers =>
+	numbers.reduce(
+		(acc, current, index, array) =>
+			index === array.length - 1
+				? (acc + current) / array.length
+				: acc + current,
+		0
+	);
